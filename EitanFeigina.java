e61a66d24a2d5bc71d515cf84fb51da2b7b84f6b
eitanFeigin9 <eitanfe@mta.ac.il>
2023-04-18 14:51:31 +0300

    my name update

--- EitanFeigina.java
+++ EitanFeigina.java
@@ -1,7 +1,7 @@
 public class EitanFeigina{
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-			System.out.println("Eitan Feigina ..."+i);
+			System.out.println("My name is: Eitan Feigina ..."+i);
 		}
 	}
 }
